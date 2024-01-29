699e9bb41e861807faf93d9f49c18cbfc8295b88
Md. Tanvir Hasan <tanvirhasaan177@gmail.com>
2024-01-30 00:12:00 +0100

    summesion and even

---
+++ sum.js
@@ -0,0 +1,22 @@
+let number = 1;
+
+let sum = 0;
+
+while(number<10){
+    sum = sum + number;
+    console.log(sum);
+
+    number++;
+
+
+
+}
+
+
+let num = 1;
+while(num <= 10){
+    if(num % 2 === 0){
+        console.log("Even Number: ", num);
+    }
+    num++;
+}
