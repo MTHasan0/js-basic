7fff55e2507ffa615a8ff42e22632b2a3454cf8a
Md. Tanvir Hasan <tanvirhasaan177@gmail.com>
2024-01-29 23:40:25 +0100

    while loop

---
+++ while.js
@@ -0,0 +1,13 @@
+const number = 7;
+
+if (number < 5){
+    console.log("Smaller number found");
+}
+console.log("Large number found");
+
+
+let num = 0;
+while(num < 5){
+    console.log("running", num)
+    num = num+1;
+}
